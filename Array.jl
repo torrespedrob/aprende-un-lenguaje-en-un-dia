d5f505083f2205a2cd2d2221f9ec9cca899e3c15
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2018-12-12 20:58:48 +0100

    array

--- Array.jl
+++ Array.jl
@@ -1,24 +1,28 @@
 a = rand(1:100, 10)
 print("Array generado: ")
 println(a)
-prim = []
-noPrim = []
+prim = Int[]
+noPrim = Int[]
 num1 = 0
 num2 = 0
-for i = 1:10
+i = 1
+while i < 10
     primo = true
-    for j = 2:a[i]
+    j = 2
+    while j == a[i]
         if (a[i] % j) == 0
             primo = false
         end
+        j = j + 1
     end
-    if primo
-        prim[num1] = a[i]
+    if primo == true
         num1 = num1 + 1
+        prim[num1] = a[i]
     else
-        noPrim[num2] = a[0, i]
         num2 = num2 + 1
+        noPrim[num2] = a[0, i]
     end
+    i = i + 1
 end
 ordenados = cat(prim, noPrim)
 println()
